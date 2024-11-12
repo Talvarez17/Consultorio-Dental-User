be1d33fa6f7914c9f249e13cba9ad875c47813a6
Tomas Alvarez <soytalvarez17@gmail.com>
2024-11-12 14:02:16 -0600

    primera carga

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RecetasComponent } from './pages/auth/recetas/recetas.component';
+import { CitasComponent } from './pages/auth/citas/citas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", children: [
+      { path: "login", component: LoginComponent },
+      { path: "citas", component: CitasComponent },
+      { path: "recetas", component: RecetasComponent },
+
+      { path: "**", redirectTo: "login" },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { CitasComponent } from './pages/auth/citas/citas.component';
+import { RecetasComponent } from './pages/auth/recetas/recetas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    CitasComponent,
+    RecetasComponent
   ],
   imports: [
     BrowserModule,
